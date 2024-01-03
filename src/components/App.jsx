49330004937a6920b9cc83f9dc426e8a40d29e3f
unknown <gtx6600@gmail.com>
2024-01-03 22:25:06 +0200

    afrerCheck

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -33,7 +33,7 @@ export const App = () => {
     <>
       <Section title="Please, leave feedback">
         <FeedbackOptions
-          options={['good', 'neutral', 'bad']}
+          options={Object.keys({good, neutral, bad})}
           onLeaveFeedback={onLeaveFeedback}
         />
       </Section>
